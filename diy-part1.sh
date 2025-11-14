0a258264bb7e375f2e55c422c3eea3aa41ee6803
edutfree <168154164+edutfree@users.noreply.github.com>
2025-11-14 15:22:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,10 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# use luci 24.10
+sed -i '/src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-23.05/s/^/#/' feeds.conf.default
+sed -i 's/^#\(src-git luci https:\/\/github.com\/coolsnowwolf\/luci.git;openwrt-24.10\)/\1/' feeds.conf.default
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
