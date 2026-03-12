d251127ba857b9be769da929117ba54322f2daca
edutfree <168154164+edutfree@users.noreply.github.com>
2026-03-12 15:13:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,9 @@
 # See /LICENSE for more information.
 #
 
-sed -i 's/192.168.1.1/192.168.11.11/g' package/base-files/luci/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.12/g' package/base-files/luci/bin/config_generate
 #sed -i 's/LEDE/OpenWrt(2025.3.21)/g' package/base-files/luci/bin/config_generate
 sed -i 's/LEDE/OpenWrt-'"$(date -d "now" +%Y-%m-%d)"'/g' package/base-files/luci/bin/config_generate
+
+sed -i 's/192.168.1.1/192.168.11.12/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OpenWrt-'"$(date -d "now" +%Y-%m-%d)"'/g' package/base-files/files/bin/config_generate
